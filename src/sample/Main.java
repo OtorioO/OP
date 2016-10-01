36fe7bbff0609584f81edb07cd472f40117ad4ec
OtorioO <comp90194@mail.ru>
2016-10-01 17:27:26 +0300

    Правки

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,32 +14,8 @@ import javafx.scene.text.*;
 import javafx.scene.control.*;
 import javafx.scene.paint.*;
 import javafx.scene.layout.HBox;
-import java.awt.Desktop;
-import java.net.URI;
-import java.net.URL;
-import java.net.URISyntaxException;
 
 public class Main extends Application {
-
-    public static void openWebpage(URI uri) {
-        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
-            try {
-                desktop.browse(uri);
-            } catch (Exception e) {
-                e.printStackTrace();
-                //far
-            }
-        }
-    }
-
-    public static void openWebpage(URL url) {
-        try {
-            openWebpage(url.toURI());
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
 
     public void FXMLDocumentController(Stage stageWindow) throws Exception {
 
@@ -48,56 +24,70 @@ public class Main extends Application {
         Scene scene = new Scene(root, 570, 200);
         stageWindow.setScene(scene);
         stageWindow.show();
-        Text sect = new Text("HelloW!");
+        Text sect = new Text("HelloWorld!");
         sect.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         root.getChildren().add(sect);
     }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Pane root = (Pane) FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Secure");
-
+    public void addObj(Pane root)
+    {
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
+
         Text scenetitle = new Text("Welcome");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0, 0, 2, 1);
+
         Label userName = new Label("User Name:");
         grid.add(userName, 0, 1);
+
         TextField userTextField = new TextField();
         grid.add(userTextField, 1, 1);
+
         Label pw = new Label("Password:");
         grid.add(pw, 0, 2);
+
         PasswordField pwBox = new PasswordField();
         grid.add(pwBox, 1, 2);
-
+        Label tip = new Label("Наведите->");
+        grid.add(tip,0,4);
         Button btn = new Button("Sign in");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4);
+        btn.setTooltip(new Tooltip("Введите:"+"\n"+"admin "+"\n"+"admin"));
 
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
+        final Label actiontarget = new Label();
+        grid.add(actiontarget, 1,5);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
-            public void handle(ActionEvent e) {
-                actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("Sign in button pressed");
-                try{Stage stageWindow = new Stage();
-                    FXMLDocumentController(stageWindow);
+            public void handle(ActionEvent e) {actiontarget.setText("");
+                if ((pwBox.getText().equals("admin")) && (userTextField.getText().equals("admin"))) {
+                    actiontarget.setText("Your password confirmed");
+                    actiontarget.setTextFill(Color.rgb(21, 117, 84));
+                    try{Stage stageWindow = new Stage();
+                        FXMLDocumentController(stageWindow);
+                    }
+                    catch(Exception ex){}}
+                else
+                {
+                    actiontarget.setText("Your data is incorrect!");
+                    actiontarget.setTextFill(Color.rgb(210, 39, 30));
                 }
-                catch(Exception ex){}
-
-                }
-        });
+                pwBox.clear();
+            }});
         root.getChildren().add(grid);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Pane root = (Pane) FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Secure");
+        addObj(root);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
@@ -105,5 +95,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        // Main f = new Main();
+        // f.start(new Stage());
     }
 }
