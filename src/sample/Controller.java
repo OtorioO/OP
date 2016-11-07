5f6e1caab2b6c331dc570ece0057758e981b5d95
OtorioO <comp90194@mail.ru>
2016-11-07 13:38:15 +0300

    Начальный интерфейс окна чата

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class Controller {
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,9 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,21 +19,88 @@ import javafx.scene.text.*;
 import javafx.scene.control.*;
 import javafx.scene.paint.*;
 import javafx.scene.layout.HBox;
+import javafx.stage.StageStyle;
+
+import java.awt.*;
 
 public class Main extends Application {
+    private static Stage primaryStageObj;
+    public void SecondWind(Stage stageWindow) throws Exception {
 
-    public void FXMLDocumentController(Stage stageWindow) throws Exception {
-
-        Pane root = (Pane) FXMLLoader.load(getClass().getResource("sample.fxml"));
-        stageWindow.setTitle("Информация");
-        Scene scene = new Scene(root, 570, 200);
+        Pane rootSec = (Pane) FXMLLoader.load(getClass().getResource("/sample/chat.fxml"));
+        stageWindow.setTitle("Чат");
+        Scene scene = new Scene(rootSec, 600, 400);
         stageWindow.setScene(scene);
         stageWindow.show();
-        Text sect = new Text("HelloWorld!");
-        sect.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        root.getChildren().add(sect);
+        chatObj(rootSec);
     }
-    public void addObj(Pane root)
+
+    public void RegWind(Stage stageWindow) throws Exception {
+
+        Pane rootReg = (Pane) FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stageWindow.initStyle(StageStyle.TRANSPARENT);
+        stageWindow.setTitle("Регистрация");
+        Scene scene = new Scene(rootReg, 300, 240);
+        stageWindow.setScene(scene);
+        stageWindow.show();
+        regObj(rootReg);
+    }
+
+    public void chatObj(Pane root)
+    {
+
+
+    }
+
+    public void regObj(Pane root)
+    {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        TextField userTextField = new TextField();
+
+        userTextField.setPromptText("Введите уникальный логин");
+        userTextField.setFocusTraversable(false);
+        grid.add(userTextField, 0, 1);
+        TextField nameTextField = new TextField();
+        nameTextField.setPromptText("Введите свое имя");
+        nameTextField.setFocusTraversable(false);
+        grid.add(nameTextField, 0, 2);
+        PasswordField  passTextField = new  PasswordField();
+        passTextField.setPromptText("Введите пароль");
+        passTextField.setFocusTraversable(false);
+        grid.add(passTextField, 0, 3);
+        PasswordField  passrepTextField = new  PasswordField();
+        passrepTextField.setPromptText("Повторите пароль");
+        passrepTextField.setFocusTraversable(false);
+        grid.add(passrepTextField, 0, 4);
+        userTextField.setPrefSize(200,nameTextField.getPrefHeight());
+
+        Button btnReg = new Button("Регистрация");
+        Button btnExit = new Button("Закрыть");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btnReg);
+        hbBtn.getChildren().add(btnExit);
+        grid.add(hbBtn, 0, 5);
+        btnExit.setOnAction(new EventHandler<ActionEvent>() {
+
+        @Override
+        public void handle(ActionEvent e) {Stage stage = (Stage) btnExit.getScene().getWindow();
+            stage.close();
+        } });
+        btnReg.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                //Послать клиенту данные о юзере
+            } });
+        root.getChildren().add(grid);
+    }
+
+    public void authObj(Pane root)
     {
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -51,43 +123,52 @@ public class Main extends Application {
 
         PasswordField pwBox = new PasswordField();
         grid.add(pwBox, 1, 2);
-        Label tip = new Label("Наведите->");
-        grid.add(tip,0,4);
-        Button btn = new Button("Sign in");
+        Button btnReg = new Button("Регистрация");
+        grid.add(btnReg,0,4);
+        Button btnAuth = new Button("Sign in");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
+        hbBtn.getChildren().add(btnAuth);
         grid.add(hbBtn, 1, 4);
-        btn.setTooltip(new Tooltip("Введите:"+"\n"+"admin "+"\n"+"admin"));
+        btnAuth.setTooltip(new Tooltip("Введите:"+"\n"+"admin "+"\n"+"admin"));
 
         final Label actiontarget = new Label();
         grid.add(actiontarget, 1,5);
 
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        btnReg.setOnAction(new EventHandler<ActionEvent>() {
+
+                                @Override
+                                public void handle(ActionEvent e) {
+                                    try{Stage stageWindowR = new Stage();
+                                        RegWind(stageWindowR);
+                                    }
+                                    catch(Exception ex){}}
+
+                            });
+
+        btnAuth.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent e) {actiontarget.setText("");
-                if ((pwBox.getText().equals("admin")) && (userTextField.getText().equals("admin"))) {
-                    actiontarget.setText("Your password confirmed");
-                    actiontarget.setTextFill(Color.rgb(21, 117, 84));
+               // if ((pwBox.getText().equals("admin")) && (userTextField.getText().equals("admin"))) {
                     try{Stage stageWindow = new Stage();
-                        FXMLDocumentController(stageWindow);
+                        SecondWind(stageWindow);
                     }
-                    catch(Exception ex){}}
-                else
-                {
+                    catch(Exception ex){System.out.println("Произошло ещё какое-то исключение"); }
+                 //} else{
                     actiontarget.setText("Your data is incorrect!");
                     actiontarget.setTextFill(Color.rgb(210, 39, 30));
-                }
+               // }
                 pwBox.clear();
             }});
         root.getChildren().add(grid);
     }
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Pane root = (Pane) FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Secure");
-        addObj(root);
+        authObj(root);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
@@ -95,7 +176,11 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        // Main f = new Main();
-        // f.start(new Stage());
+    }
+    public static Stage getPrimaryStage() {
+        return primaryStageObj;
     }
 }
+
+
+
